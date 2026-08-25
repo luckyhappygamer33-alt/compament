edfbee2945cfb1d978f1d6f5c5e8bcf9b08295ac
luckyhappygamer33-alt <luckyhappygamer33@gmail.com>
2026-08-25 09:29:19 +0200

    refactor: remove selectedElementId and setSeelctedElement

--- src/components/PropertiesPanel/PropertiesPanel.tsx
+++ src/components/PropertiesPanel/PropertiesPanel.tsx
@@ -1,6 +1,5 @@
 import './PropertiesPanel.css'
 import { useEditorStore } from '../../store/editorStore'
-import type { Element } from '../../types/schema'
 import { buildMasterPropertiesObject } from './MasterPropertiesObjectBuilder'
 import { resolvePropertyStates, type PropertyState } from './PropertyStatesResolver'
 
@@ -55,11 +54,9 @@ function addPropertiesLevel(
 
 export default function PropertiesPanel({ onBake }: { onBake: () => void }) {
     const layers = useEditorStore(state => state.layers)
-    const selectedElementId = useEditorStore(state => state.selectedElementId)
     const selectedElementIds = useEditorStore(state => state.selectedElementIds)
     const deleteElement = useEditorStore(state => state.deleteElement)
-    const setSelectedElement = useEditorStore(state => state.setSelectedElement)
-
+    const setSelectedElements = useEditorStore(state => state.setSelectedElements)
     const selectedElementIdsSet = new Set(selectedElementIds)
 
     const selectedElements = layers.flatMap(layer =>
@@ -70,26 +67,6 @@ export default function PropertiesPanel({ onBake }: { onBake: () => void }) {
 
     const masterPropertiesObject = buildMasterPropertiesObject(selectedElements)
     const propertyStates = resolvePropertyStates(selectedElements, masterPropertiesObject)
-
-    console.log('propertyStates:', propertyStates)
-
-    // Find the selected element and the layer containing it.
-    let selectedElement: Element | null = null
-    let selectedLayerId: string | null = null
-
-    if (selectedElementId) {
-        for (const layer of layers) {
-            const element = layer.elements.find(
-                element => element.id === selectedElementId
-            )
-
-            if (element) {
-                selectedElement = element
-                selectedLayerId = layer.id
-                break
-            }
-        }
-    }
 
     return (
         <div className='properties-panel'>
@@ -123,9 +100,14 @@ export default function PropertiesPanel({ onBake }: { onBake: () => void }) {
                                     type='button'
                                     className='element-action-button'
                                     onClick={() => {
-                                        if (!selectedElement || !selectedLayerId) return
-                                        deleteElement(selectedLayerId, selectedElement.id)
-                                        setSelectedElement(null)
+                                        for (const layer of layers) {
+                                            for (const element of layer.elements) {
+                                                if (selectedElementIdsSet.has(element.id)) {
+                                                    deleteElement(layer.id, element.id)
+                                                }
+                                            }
+                                        }
+                                        setSelectedElements([])
                                     }}
                                 >
                                     Delete
